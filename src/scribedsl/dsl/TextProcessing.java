25918c2c5e4dd840dc144e3a528550aabb118f7b
AnaLiliaTeixeira <ana.lilia.teixeira2@gmail.com>
2024-03-26 17:28:21 +0000

    Created a lot of tests to cover everythinh needed

--- src/scribedsl/dsl/TextProcessing.java
+++ src/scribedsl/dsl/TextProcessing.java
@@ -1,7 +1,6 @@
 package scribedsl.dsl;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -99,7 +98,7 @@ public class TextProcessing {
 				StringBuilder sb = new StringBuilder();
 				String line;
 				while ((line = br.readLine()) != null) {
-					sb.append(line);
+					sb.append(line).append(" ");
 				}
 				withData(sb.toString());
 			} catch (IOException e) {
--- test/scribedsl/ScribedslTest.java
+++ test/scribedsl/ScribedslTest.java
@@ -8,71 +8,142 @@ import scribedsl.dsl.TextProcessing;
 public class ScribedslTest {
 
 	public static void main(String[] args) {
-		
-		ScribedslFactory factory = ScribedslFactory.eINSTANCE;
-		
-		//1. Create the processor
-//		Processor processor = factory.createProcessor();
-		
-		//2. Create an instance of a processed data
-//		ProcessedData processedData = factory.createProcessedData();
-		//3. Create one text
-//		processor.setText(text);
-//		Text text = factory.createText();
-//		text.setValue("This is an example to be processed");
-//				
-//		System.out.println("This is the text: " + text.getValue());
-		
+
 		TextProcessing processor = new TextProcessing();
-		ProcessedData analyseText = processor.withData("This is an example text to be processed")
-											 .tokenize("\\s+")
-											 .build();
-		
-		System.out.println("Text: " + analyseText.getProcessor().getText().getValue());
-		List<Token> tokens = analyseText.getProcessor().getToken();
+		String text = "This is an example text to be processed";
+
+		// Test 1 - Tokenize text
+		ProcessedData tokenizeText = processor.withData(text).tokenize("\\s+").build();
+
+		System.out.println("===== Test 1: Tokenize text =====");
+		System.out.print("Token list: [");
+		List<Token> tokens = tokenizeText.getProcessor().getToken();
 		for (Token t : tokens) {
-			System.out.println(t.getValue());
+			System.out.print("\"" + t.getValue() + "\", ");
+//			tokens.indexOf(t) == tokens.size() ? System.out.println(); : System.out.print(",");
 		}
-		System.out.println();
-		
-		ProcessedData analyseText2 = processor.withData("This is an example text to be processed like an example")
-				 .tokenize("\\s+")
-				 .filterStopWords()
-				 .performStemming()
-				 .analyseWordFrequency()
-				 .build();
-		
-		System.out.println();
-		
-		System.out.println("Text: " + analyseText2.getProcessor().getText().getValue());
-		List<Token> tokens2 = analyseText2.getProcessor().getToken();
-		for (Token t : tokens2) {
-			System.out.println(t.getValue());
+		System.out.print("]\n");
+
+		// Test 2 - Tokenize text and filter stop words
+		ProcessedData filterStopWords = processor.withData(text).tokenize("\\s+").filterStopWords().build();
+
+		System.out.println("\n===== Test 2: Tokenize text and filter stop words =====");
+		System.out.print("Token list: [");
+		tokens = filterStopWords.getProcessor().getToken();
+		for (Token t : tokens) {
+			System.out.print("\"" + t.getValue() + "\", ");
 		}
-		
-		System.out.println("Analysing word frequency:");
-		for (Entry<String, Integer> e : analyseText2.getProcessor().getWordfrequency()) {
-			System.out.println("Key = " + e.getKey() + ", Value = " + e.getValue());
+		System.out.print("]\n");
+
+		// Test 3 - Tokenize text and perform stemming
+		ProcessedData performStemming = processor.withData(text).tokenize("\\s+").performStemming().build();
+
+		System.out.println("\n===== Test 3: Tokenize text and perform stemming =====");
+		System.out.print("Token list: [");
+		tokens = performStemming.getProcessor().getToken();
+		for (Token t : tokens) {
+			System.out.print("\"" + t.getValue() + "\", ");
 		}
-		
-		System.out.println();
-		
-		ProcessedData analyseText3 = processor.fromFile("input.txt")
-				 .tokenize("\\s+")
-				 .filterStopWords()
-				 .performStemming()
-				 .analyseWordFrequency()
-				 .build();
-		
-		System.out.println("Text: " + analyseText3.getProcessor().getText().getValue());
-		List<Token> tokens3 = analyseText3.getProcessor().getToken();
-		for (Token t : tokens3) {
-			System.out.println(t.getValue());
+		System.out.print("]\n");
+
+		// Test 4 - Tokenize text and analyse word frequency
+		ProcessedData analyseWordFrequency = processor.withData(text).tokenize("\\s+").analyseWordFrequency().build();
+
+		System.out.println("\n===== Test 4: Tokenize text and analyse word frequency =====");
+		System.out.print("Token list: [");
+		tokens = analyseWordFrequency.getProcessor().getToken();
+		for (Token t : tokens) {
+			System.out.print("\"" + t.getValue() + "\", ");
 		}
-		
-		System.out.println("Analysing word frequency:");
-		for (Entry<String, Integer> e : analyseText3.getProcessor().getWordfrequency()) {
-			System.out.println("Key = " + e.getKey() + ", Value = " + e.getValue());
+		System.out.print("]\n");
+		System.out.print("Analyse word frequency: [");
+		for (Entry<String, Integer> e : analyseWordFrequency.getProcessor().getWordfrequency()) {
+			System.out.print("(\"" + e.getKey() + "\" " + e.getValue() + "), ");
 		}
+		System.out.print("]\n");
+
+		// Test 5 - Tokenize text, filter stop words and perform stemming
+		ProcessedData stopWordsStemming = processor.withData(text).tokenize("\\s+").filterStopWords().performStemming()
+				.build();
+
+		System.out.println("\n===== Test 5: Tokenize text, filter stop words and perform stemming =====");
+		System.out.print("Token list: [");
+		tokens = stopWordsStemming.getProcessor().getToken();
+		for (Token t : tokens) {
+			System.out.print("\"" + t.getValue() + "\", ");
+		}
+		System.out.print("]\n");
+
+		// Test 6 - Tokenize text, filter stop words and analyse word frequency
+		ProcessedData stopWordsFrequency = processor.withData(text).tokenize("\\s+").filterStopWords()
+				.analyseWordFrequency().build();
+
+		System.out.println("\n===== Test 6: Tokenize text, filter stop words and analyse word frequency =====");
+		System.out.print("Token list: [");
+		tokens = stopWordsFrequency.getProcessor().getToken();
+		for (Token t : tokens) {
+			System.out.print("\"" + t.getValue() + "\", ");
+		}
+		System.out.print("]\n");
+		System.out.print("Analyse word frequency: [");
+		for (Entry<String, Integer> e : stopWordsFrequency.getProcessor().getWordfrequency()) {
+			System.out.print("(\"" + e.getKey() + "\" " + e.getValue() + "), ");
+		}
+		System.out.print("]\n");
+
+		// Test 7 - Tokenize text, perform stemming and analyse word frequency
+		ProcessedData stemmingFrequency = processor.withData(text).tokenize("\\s+").performStemming()
+				.analyseWordFrequency().build();
+
+		System.out.println("\n===== Test 7: Tokenize text, perform stemming and analyse word frequency =====");
+		System.out.print("Token list: [");
+		tokens = stemmingFrequency.getProcessor().getToken();
+		for (Token t : tokens) {
+			System.out.print("\"" + t.getValue() + "\", ");
+		}
+		System.out.print("]\n");
+		System.out.print("Analyse word frequency: [");
+		for (Entry<String, Integer> e : stemmingFrequency.getProcessor().getWordfrequency()) {
+			System.out.print("(\"" + e.getKey() + "\" " + e.getValue() + "), ");
+		}
+		System.out.print("]\n");
+
+		// Test 8 - Tokenize text, filter stop words, perform stemming and analyse word
+		// frequency
+		ProcessedData analyseText = processor.withData(text).tokenize("\\s+").filterStopWords().performStemming()
+				.analyseWordFrequency().build();
+
+		System.out.println("\n===== Test 6: Tokenize text, filter stop words and analyse word frequency =====");
+		System.out.print("Token list: [");
+		tokens = analyseText.getProcessor().getToken();
+		for (Token t : tokens) {
+			System.out.print("\"" + t.getValue() + "\", ");
+		}
+		System.out.print("]\n");
+		System.out.print("Analyse word frequency: [");
+		for (Entry<String, Integer> e : analyseText.getProcessor().getWordfrequency()) {
+			System.out.print("(\"" + e.getKey() + "\", " + e.getValue() + "), ");
+		}
+		System.out.print("]\n");
+
+		// Test 9 - Read a text from a file, tokenize the text, filter stop words,
+		// perform stemming and analyse word frequency
+		ProcessedData textFromFile = processor.fromFile("input.txt").tokenize("\\s+").filterStopWords()
+				.performStemming().analyseWordFrequency().build();
+
+		System.out.println(
+				"\n===== Test 9: Read a text from a file, tokenize the text, filter stop words, perform stemming and analyse word frequency =====");
+		System.out.print("Token list: [");
+		tokens = textFromFile.getProcessor().getToken();
+		for (Token t : tokens) {
+			System.out.print("\"" + t.getValue() + "\", ");
+		}
+		System.out.print("]\n");
+		System.out.print("Analyse word frequency: [");
+		for (Entry<String, Integer> e : textFromFile.getProcessor().getWordfrequency()) {
+			System.out.print("(\"" + e.getKey() + "\", " + e.getValue() + "), ");
+		}
+		System.out.print("]\n");
+
 	}
 }
